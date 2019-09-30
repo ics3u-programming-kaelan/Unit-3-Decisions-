afe61a4393be0c834b80e960256bca86861f4231
Kaelan Khan <32547155+KaelanKhan@users.noreply.github.com>
2019-09-30 13:08:42 -0400

    Added guess number

---
+++ GuessNumberKaelanK/GuessNumberKaelanK/GuessNumberForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace GuessNumberKaelanK
+{
+    partial class frmGuessNumber
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGuessNumber = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblGuessNumber
+            // 
+            this.lblGuessNumber.AutoSize = true;
+            this.lblGuessNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuessNumber.Location = new System.Drawing.Point(81, 73);
+            this.lblGuessNumber.Name = "lblGuessNumber";
+            this.lblGuessNumber.Size = new System.Drawing.Size(305, 24);
+            this.lblGuessNumber.TabIndex = 0;
+            this.lblGuessNumber.Text = "Guess a number between 1 and 10";
+            this.lblGuessNumber.Click += new System.EventHandler(this.LblGuessNumber_Click);
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(432, 77);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(153, 20);
+            this.txtGuess.TabIndex = 1;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(129, 215);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "button1";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            // 
+            // frmGuessNumber
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.lblGuessNumber);
+            this.Name = "frmGuessNumber";
+            this.Text = "Guess Number by Kaelan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGuessNumber;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ GuessNumberKaelanK/GuessNumberKaelanK/GuessNumberForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessNumberKaelanK
+{
+    public partial class frmGuessNumber : Form
+    {
+        public frmGuessNumber()
+        {
+            InitializeComponent();
+        }
+
+        private void LblGuessNumber_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
