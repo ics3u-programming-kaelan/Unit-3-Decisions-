805dd6ed6b8d925dea4de1371f154294feddaa56
Kaelan Khan <32547155+KaelanKhan@users.noreply.github.com>
2019-10-04 12:14:03 -0400

    guess number

--- GuessNumberKaelanK/GuessNumberKaelanK/GuessNumberForm.Designer.cs
+++ GuessNumberKaelanK/GuessNumberKaelanK/GuessNumberForm.Designer.cs
@@ -31,6 +31,7 @@
             this.lblGuessNumber = new System.Windows.Forms.Label();
             this.txtGuess = new System.Windows.Forms.TextBox();
             this.btnCheck = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblGuessNumber
@@ -53,18 +54,29 @@
             // 
             // btnCheck
             // 
-            this.btnCheck.Location = new System.Drawing.Point(129, 215);
+            this.btnCheck.Location = new System.Drawing.Point(99, 143);
             this.btnCheck.Name = "btnCheck";
-            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.Size = new System.Drawing.Size(87, 32);
             this.btnCheck.TabIndex = 2;
-            this.btnCheck.Text = "button1";
+            this.btnCheck.Text = "Check Size";
             this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(457, 162);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(35, 13);
+            this.lblAnswer.TabIndex = 3;
+            this.lblAnswer.Text = "label1";
             // 
             // frmGuessNumber
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAnswer);
             this.Controls.Add(this.btnCheck);
             this.Controls.Add(this.txtGuess);
             this.Controls.Add(this.lblGuessNumber);
@@ -80,6 +92,7 @@
         private System.Windows.Forms.Label lblGuessNumber;
         private System.Windows.Forms.TextBox txtGuess;
         private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblAnswer;
     }
 }
 
--- GuessNumberKaelanK/GuessNumberKaelanK/GuessNumberForm.cs
+++ GuessNumberKaelanK/GuessNumberKaelanK/GuessNumberForm.cs
@@ -15,11 +15,40 @@ namespace GuessNumberKaelanK
         public frmGuessNumber()
         {
             InitializeComponent();
+            lblAnswer.Hide();
         }
 
         private void LblGuessNumber_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            const int MAX_NUMBER = 10;
+            int numNumbers;
+
+            numNumbers = int.Parse(txtGuess.Text);
+
+            if (numNumbers > MAX_NUMBER)
+            {
+                lblAnswer.Text = "Your Guess Is Too High";
+                lblAnswer.Show();
+            }
+
+            if (numNumbers < MAX_NUMBER)
+            {
+                lblAnswer.Text = "Your Guess Is Too Low";
+                lblAnswer.Show();
+            }
+
+            else
+            {
+                lblAnswer.Text = "You Guessed Right!";
+                lblAnswer.Show();
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"n:\Classes\Sem1-Fall2019\ICS3U Programming\KKaelan (s254351)\Unit 3 (Decisions)\GuessNumberKaelanK\Sounds\Correct Answer Sound Effect.mp3");
+                player.Play();
+            }
         }
     }
 }
