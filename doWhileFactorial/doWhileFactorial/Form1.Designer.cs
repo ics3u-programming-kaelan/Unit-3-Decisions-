66ba543e2014aa8580a838ba43e54d7077e280e7
Kaelan Khan <32547155+KaelanKhan@users.noreply.github.com>
2019-10-09 12:32:26 -0400

    Added do While Factorial

---
+++ doWhileFactorial/doWhileFactorial/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace doWhileFactorial
+{
+    partial class frmFactorial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtUserValue = new System.Windows.Forms.TextBox();
+            this.lblUserValue = new System.Windows.Forms.Label();
+            this.lstFactorialNumbers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(92, 173);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // txtUserValue
+            // 
+            this.txtUserValue.Location = new System.Drawing.Point(247, 117);
+            this.txtUserValue.Name = "txtUserValue";
+            this.txtUserValue.Size = new System.Drawing.Size(67, 20);
+            this.txtUserValue.TabIndex = 1;
+            // 
+            // lblUserValue
+            // 
+            this.lblUserValue.AutoSize = true;
+            this.lblUserValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserValue.Location = new System.Drawing.Point(88, 117);
+            this.lblUserValue.Name = "lblUserValue";
+            this.lblUserValue.Size = new System.Drawing.Size(123, 20);
+            this.lblUserValue.TabIndex = 2;
+            this.lblUserValue.Text = "Enter a number:";
+            // 
+            // lstFactorialNumbers
+            // 
+            this.lstFactorialNumbers.FormattingEnabled = true;
+            this.lstFactorialNumbers.Location = new System.Drawing.Point(92, 226);
+            this.lstFactorialNumbers.Name = "lstFactorialNumbers";
+            this.lstFactorialNumbers.Size = new System.Drawing.Size(223, 212);
+            this.lstFactorialNumbers.TabIndex = 3;
+            // 
+            // frmFactorial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstFactorialNumbers);
+            this.Controls.Add(this.lblUserValue);
+            this.Controls.Add(this.txtUserValue);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "frmFactorial";
+            this.Text = "Do While Factorial Kaelan K";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtUserValue;
+        private System.Windows.Forms.Label lblUserValue;
+        private System.Windows.Forms.ListBox lstFactorialNumbers;
+    }
+}
+
---
+++ doWhileFactorial/doWhileFactorial/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace doWhileFactorial
+{
+    public partial class frmFactorial : Form
+    {
+        public frmFactorial()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            Double factorialAnswer = 1;
+            Double factorialUserValue;
+            int factorialCounter = 1;
+
+            this.lstFactorialNumbers.Items.Clear();
+
+
+            factorialUserValue = Convert.ToDouble(this.txtUserValue.Text);
+
+            do
+            {
+                factorialCounter = factorialCounter + 1;
+            } while ();
+
+        }
+    }
+}
