78777465836e9bcaed456e761aecff9305148ca5
Kaelan Khan <32547155+KaelanKhan@users.noreply.github.com>
2019-10-16 12:00:19 -0400

    Finished guess random number

---
+++ GuessRandomNumKaelanK/GuessRandomNumKaelanK/GuessRandomNumForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace GuessRandomNumKaelanK
+{
+    partial class frmGuessRandomNum
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblText = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText.Location = new System.Drawing.Point(67, 98);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(266, 20);
+            this.lblText.TabIndex = 0;
+            this.lblText.Text = "Guess an integer between 1 and 10:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(353, 98);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 1;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(71, 178);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(103, 34);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(222, 184);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(36, 20);
+            this.lblOutput.TabIndex = 3;
+            this.lblOutput.Text = "???";
+            // 
+            // frmGuessRandomNum
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblText);
+            this.Name = "frmGuessRandomNum";
+            this.Text = "Guess Random Num by Kaelan K";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ GuessRandomNumKaelanK/GuessRandomNumKaelanK/GuessRandomNumForm.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Created by: Kaelan K
+ * Created on: 16-10-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #17 - Random Number
+ * This program generates a random number for you to guess
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessRandomNumKaelanK
+{
+    public partial class frmGuessRandomNum : Form
+    {
+        // declare global and local vars and consts
+        int correctGuess;
+        Random randomNumberGenerator = new Random();
+        const int MIN_VALUE = 1;
+        const int MAX_VALUE = 10;
+
+        public frmGuessRandomNum()
+        {
+            InitializeComponent();
+
+            // hide the label initially
+            lblOutput.Hide();
+
+            // setting correct guess
+            correctGuess = randomNumberGenerator.Next(MIN_VALUE, MAX_VALUE + 1);
+        }
+
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            // declare the constants and vars
+            const int MIN_NUM = 1;
+            const int MAX_NUM = 10;
+            int aRandomNumber;
+            int userValue;
+            Random randomNumberGenerator = new Random();
+
+            userValue = int.Parse(txtInput.Text);
+
+            // get the random number
+            aRandomNumber = randomNumberGenerator.Next(MIN_NUM, MAX_NUM + 1);
+
+            // let the user know if the guessed right or not
+            if (userValue == aRandomNumber)
+            {
+                lblOutput.Text = "You guessed right";
+                lblOutput.Show();
+            }
+            else
+            {
+                lblOutput.Text = Convert.ToString("You guessed wrong, the answer was " + aRandomNumber);
+                lblOutput.Show();
+            }
+
+        }
+    }
+}
