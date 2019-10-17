f4a0a7c9cb10b560fdb84d8533de468b0df18e51
Kaelan Khan <32547155+KaelanKhan@users.noreply.github.com>
2019-10-17 12:56:52 -0400

    perfect square

--- PerfectSquareWhileLoop/PerfectSquareWhileLoop/FormPerfectSquareLoop.cs
+++ PerfectSquareWhileLoop/PerfectSquareWhileLoop/FormPerfectSquareLoop.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Kaelan K
+ * Created on: 17-10-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #22 - Perfect Squares with While Loop
+ * This program calculates and displays perfect squares
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -44,11 +51,16 @@ namespace PerfectSquareWhileLoop
                 squareRootAsInterger = Convert.ToInt32(squareRootAsDouble);
 
                 // the only way for the two numbers to be the same is if they are a perfect square
-                if  (squareRootAsInterger == squareRootAsInterger)
+                if  (squareRootAsInterger == squareRootAsDouble)
                 {
                     this.lstPerfectSquare.Items.Add(value + " is a perfect square");
+                    
+                    // refresh the form
                     this.Refresh();
                 }
+
+                // increases value by 1
+                value++;
             }
 
         }
