9d2eb2719de46cbd1bf32058e27fd4507a58a398
Kaelan Khan <32547155+KaelanKhan@users.noreply.github.com>
2019-10-21 12:39:47 -0400

    Unicode

---
+++ UnicodeKaelanK/UnicodeKaelanK/UnicodeForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace UnicodeKaelanK
+{
+    partial class frmUnicode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstValues = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstValues
+            // 
+            this.lstValues.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstValues.FormattingEnabled = true;
+            this.lstValues.ItemHeight = 20;
+            this.lstValues.Location = new System.Drawing.Point(34, 133);
+            this.lstValues.Name = "lstValues";
+            this.lstValues.Size = new System.Drawing.Size(189, 164);
+            this.lstValues.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(80, 53);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(99, 36);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // frmUnicode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(258, 332);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lstValues);
+            this.Name = "frmUnicode";
+            this.Text = "Unicode by Kaelan K";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox lstValues;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ UnicodeKaelanK/UnicodeKaelanK/UnicodeForm.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Created by: Kaelan K
+ * Created on: 21-10-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #24 - Unicode
+ * This program displays the decimal value of letters
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnicodeKaelanK
+{
+    public partial class frmUnicode : Form
+    {
+        public frmUnicode()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            // declare the local vars
+            int counter;
+            string alphaValue;
+
+            // clear the items in the list box
+            this.lstValues.Items.Clear();
+            
+            // set counter to 65, loop until less or equal to 90, add one to counter
+            for (counter = 65; counter <= 90; counter++)
+            {
+                // convert counter to letter
+                alphaValue = Char.ConvertFromUtf32(counter);
+
+                // display the letter and the counter value
+                this.lstValues.Items.Add(alphaValue + " -> " + counter);
+            }
+
+        }
+    }
+}
