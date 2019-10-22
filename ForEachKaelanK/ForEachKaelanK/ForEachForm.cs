5a3d4a331d3032756e6343716b4c594525011945
Kaelan Khan <32547155+KaelanKhan@users.noreply.github.com>
2019-10-22 12:16:09 -0400

    For Each Broken

---
+++ ForEachKaelanK/ForEachKaelanK/ForEachForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForEachKaelanK
+{
+    public partial class frmForEach : Form
+    {
+        public frmForEach()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnColour_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        foreach (Control aControlObject in this.Controls)
+        {
+            aControlObject.BackColor = Color.Green;
+        }
+
+        if (aControlObject.GetType() == typeof(Label))
+        {
+            aControlObject.BackColor = Color.Purple;
+        }
+    }
+}
