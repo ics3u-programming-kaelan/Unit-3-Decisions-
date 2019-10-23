9bae3470dc8ffada3ed25a6710ab334604f74f5c
Kaelan Khan <32547155+KaelanKhan@users.noreply.github.com>
2019-10-23 13:07:41 -0400

    nested loops

---
+++ nestedLoopsKaelanK/nestedLoopsKaelanK/NestedLoopForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace nestedLoopsKaelanK
+{
+    partial class frmNestedLoop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstAlphaNest = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(317, 65);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // lstAlphaNest
+            // 
+            this.lstAlphaNest.FormattingEnabled = true;
+            this.lstAlphaNest.Location = new System.Drawing.Point(12, 83);
+            this.lstAlphaNest.Name = "lstAlphaNest";
+            this.lstAlphaNest.Size = new System.Drawing.Size(317, 355);
+            this.lstAlphaNest.TabIndex = 1;
+            // 
+            // frmNestedLoop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 450);
+            this.Controls.Add(this.lstAlphaNest);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmNestedLoop";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstAlphaNest;
+    }
+}
+
---
+++ nestedLoopsKaelanK/nestedLoopsKaelanK/NestedLoopForm.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Created by: First Last
+ * Created on: Day-Month-Year
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #28 - Alphabet Nested Loops
+ * This program...
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nestedLoopsKaelanK
+{
+    public partial class frmNestedLoop : Form
+    {
+        public frmNestedLoop()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            // declare local vars
+            int alphaVal;
+            int lowAlphaVal;
+            string letter;
+            string lowLetter;
+
+            // clear listbox
+            this.lstAlphaNest.Items.Clear();
+
+            // loop through upper case alpha
+            for (alphaVal = 65; alphaVal <= 90; alphaVal++)
+            {
+                letter = Char.ConvertFromUtf32(alphaVal);
+
+                // loop through lower case
+                for (lowAlphaVal = 97; lowAlphaVal <= 122; lowAlphaVal++)
+                {
+                    lowLetter = Char.ConvertFromUtf32(lowAlphaVal);
+                    this.lstAlphaNest.Items.Add(lowAlphaVal + " -> " + lowLetter);
+                    
+                }
+                this.lstAlphaNest.Items.Add(alphaVal + " -> " + letter);
+            }
+
+        }
+    }
+}
